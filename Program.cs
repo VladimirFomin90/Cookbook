5d29837d63ce9009d28043f45b065dae90a4b808
VladimirFomin90 <vladimirfomin90@yandex.ru>
2024-03-04 23:50:27 +0500

    Добавлен каркас приложения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var cookbookApp = new CookbookApp();
+
+cookbookApp.Run();
+
+public class CookbookApp
+{
+    private readonly RecipeRepository _recipeRepository;
+    private readonly RecipeUserInput _recipeUserInput;
+
+    public CookbookApp(RecipeRepository _recipeRepository, RecipeUserInput _recipeUserInput)
+    {
+        _recipeRepository = recipeRepository;
+        _recipeUserInput = recipeUserInput;
+    }
+
+    public void Run()
+    {
+        var allRecipe = _recipeRepository.Read(filePath);
+        _recipeUserInput.PrintExistRecipe(allRecipe);
+
+        _recipeUserInput.CreateRecipe();
+
+        var ingredient = _recipeUserInput.ReadIngredientFromUser();
+
+        if (ingredient.Count > 0)
+        {
+            var recipe = new Recipe(ingredient);
+            allRecipe.AddRecipe(recipe);
+            _recipeRepository.Save(filePath, allRecipe);
+
+            _recipeUserInput.ShowMessage("Рецепт добавлен:");
+            _recipeUserInput.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _recipeUserInput.ShowMessage("Не были выбраны ингредиенты");
+        }
+
+        _recipeUserInput.Exit();
+    }
+}
+
+public class RecipeUserInput
+{
+}
+
+public class RecipeRepository
+{
+}
